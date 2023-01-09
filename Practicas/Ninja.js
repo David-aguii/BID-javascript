ded10c24f2e02fd8c52c342ac1fc91773f7e8c93
David-aguii <davidaguii1927@gmail.com>
2023-01-09 18:36:17 -0300

    Nuevos Cambios

---
+++ Practicas/Ninja.js
@@ -0,0 +1,21 @@
+class Ninja{
+    constructor(nombre,salud){
+        this.nombre=nombre;
+        this.salud=salud;
+        this.velocidad=3;
+        this.fuerza=3;
+    }
+    sayName(){
+        console.log("El nombre del nuevo ninja es "+ this.nombre);
+    }
+    showStats(){
+        console.log("Nombre:"+ this.nombre +"\nFuerza:"+this.fuerza+"\nSalud:"+ this.salud);
+    }
+    drinkSake(){
+        this.salud+=10;
+    }
+}
+const ninja1= new Ninja("Hasbulla",10);
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
